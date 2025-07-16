0fd72917e806720f6fa3ed2dff63c8af1d7b3f7a
AristidesMoreira <20251ctb0100063@estudantes.ifpr.edu.br>
2025-07-16 12:28:54 -0300

    Aula 16/07

--- exercicios/script.js
+++ exercicios/script.js
@@ -1,21 +1,58 @@
 // Exercicio - 1
-function somar (){
+function somar() {
     let n1 = Number(document.getElementById("num1").value);
     let n2 = Number(document.getElementById("num2").value);
     // console.log("Resultado: "+ (n1+n2));
 
-    document.getElementById("resultado1").innerText = "Resultado: " + (n1+n2);
+    document.getElementById("resultado1").innerText = "Resultado: " + (n1 + n2);
 }
 
 // Exercicio - 2
-function calcaularidade (){
+function calcularidade() {
     let n1 = Number(document.getElementById("idade1").value);
     let resultado;
-    if (n1 >= 18){
-        resultado = "Você é de maior"
-    } else{
-        "Você é de menor"
+    if (n1 >= 18) {
+        resultado = "Você é de maior";
+    } else {
+        resultado = "Você é de menor";
     }
 
-    document.getElementById("resultado").innerText = resultado;
+    document.getElementById("resultado2").innerText = resultado;
 }
+// Exercicio - 3
+function contarCaracteres() {
+    let t1 = (document.getElementById("texto1").value);
+    let tamanho = t1.length;
+
+    document.getElementById("resultado3").innerText = tamanho;
+}
+// Exercicio - 4
+function parOuImpar() {
+    let n3 = Number(document.getElementById("n4").value);
+    let parouimpar = "";
+    if (n3 % 2 === 0) {
+        parouimpar =  "Par";
+    } else {
+        parouimpar = "Ímpar";
+    }
+
+    document.getElementById("resultado4").innerText = parouimpar;
+}
+// Exercicio - 5
+function caixaAlta() {
+    let t5 = (document.getElementById("texto5").value);
+    let textoMaiusculo = t5.toUpperCase();
+
+    
+    document.getElementById("resultado5").innerText = textoMaiusculo;
+}
+// Exercicio - 6
+function caixaAlta() {
+    let n5 = (document.getElementById("numero6").value);
+    let textoMaiusculo = t5.toUpperCase();
+
+    
+    document.getElementById("resultado5").innerText = textoMaiusculo;
+}
+
+
